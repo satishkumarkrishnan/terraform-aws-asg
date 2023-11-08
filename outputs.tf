5a60c465a9143befedca0970dc6cbc0122e4d4ed
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-08 15:43:32 +0530

    added ec2 instnace id to the output.tf

--- outputs.tf
+++ outputs.tf
@@ -24,6 +24,6 @@ output "asg_alb_hosted_zone_id" {
 
 
 output "ec2_InstanceId" {
-    value = module.vpc.instance[0].id
+    value = data.aws_instances.tokyo_instances[0].id
     description = "get ec2 instance value id"
 }
