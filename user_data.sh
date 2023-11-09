4d8440230ea144c2a964385d8bfff99e0145f2f7
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-09 12:33:42 +0530

    Added the user data to create ec2 tags

--- user_data.sh
+++ user_data.sh
@@ -9,13 +9,9 @@
      #CURRENT_NAME=$(aws ec2 describe-tags --filters Name=resource-id,Values=${ID} Name=key,Values=Name --query Tags[].Value --output text
      #NEW_NAME=${CURRENT_NAME}-${ID}
      #aws ec2 create-tags --resources ${ID} --tags Key=Name,Value=${NEW_NAME}
-#i=1
-#for INSTANCE in $(aws autoscaling describe-auto-scaling-instances --query 'AutoScalingInstances[*].[InstanceId]' --output text)
-#do
-#aws ec2 create-tags --resources $INSTANCE --tags Key=Name,Value="tokyo_ec2_instance"$i
-#i=$((i+1))
-#done
-ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-CURRENT_NAME=$(aws ec2 describe-tags --filters Name=resource-id,Values=${ID} Name=key,Values=Name --query Tags[].Value --output text
-NEW_NAME=${CURRENT_NAME}-${ID}
-aws ec2 create-tags --resources ${ID} --tags Key=Name,Value=${NEW_NAME}
+i=1
+for instance in $(aws autoscaling describe-auto-scaling-instances --query AutoScalingInstances[].InstanceId --output text);
+do 
+aws ec2 create-tags --resources $instance --tags Key=Name,Value=myInstance-$i
+i=$((i+1));
+done
