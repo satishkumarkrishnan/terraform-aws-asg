2e1ea18c4742f5dd3a7a4fec22e1168e6910c1f6
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-10 13:10:15 +0530

    Added the null resource block

--- tag.sh
+++ tag.sh
@@ -2,8 +2,6 @@
 i=1
 for INSTANCE in $(aws autoscaling describe-auto-scaling-instances --query AutoScalingInstances[].InstanceId --output text);
 do
-echo $INSTANCE	
 aws ec2 create-tags --resources $INSTANCE --tags Key=Name,Value="tokyo_instance"$i
 i=$((i+1));
-echo $INSTANCE	
 done
