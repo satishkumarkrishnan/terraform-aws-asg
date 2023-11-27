04916b5b5ebd830dcd02d583232ed84c9f5ea34e
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-27 15:59:37 +0530

    updated dir

--- efs_mount.sh
+++ efs_mount.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo su
-mkdir /access/tokyo-efs-mount
+mkdir /tokyo-efs-mount
 # Mounting Efs 
 sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  /access/tokyo-efs-mount
 # Making Mount Permanent
