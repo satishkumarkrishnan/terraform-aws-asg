3dc04e6769662c17b80b806ae6e02cb9b060f896
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-27 18:26:58 +0530

    updated user data script

--- main.tf
+++ main.tf
@@ -34,7 +34,6 @@ resource "aws_launch_template" "tokyo_launch_template" {
 			      mkdir /tokyo-efs-mount
 			      # Mounting Efs 
             sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.tokyo_efs.dns_name}:/  /access/tokyo-efs-mount
-#			      sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  /tokyo-efs-mount
 			      # Making Mount Permanent
 			      echo ${aws_efs_file_system.tokyo_efs.dns_name}:/ /var/www/html nfs4 defaults,_netdev 0 0  | sudo cat >> /etc/fstab
             EOF
