c867b6fd777875759adb88d44b5b463193b7b68b
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-28 17:14:03 +0530

    removed the sudo access

--- efs_mount.sh
+++ efs_mount.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo su
 mkdir /tokyo-efs-mount
 # Mounting Efs 
 sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  /tokyo-efs-mount
