68245de9619898258d03d3d9be378f2ecfee72e5
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-28 16:44:56 +0530

    removed the access mount name

--- efs_mount.sh
+++ efs_mount.sh
@@ -2,6 +2,6 @@
 sudo su
 mkdir /tokyo-efs-mount
 # Mounting Efs 
-sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  /access/tokyo-efs-mount
+sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  /tokyo-efs-mount
 # Making Mount Permanent
 echo ${efs_hostname}:/ /var/www/html nfs4 defaults,_netdev 0 0  | sudo cat >> /etc/fstab
