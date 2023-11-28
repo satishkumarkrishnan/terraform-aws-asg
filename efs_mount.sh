cb4a66196a264bc244323a419b5d00cffbebd404
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-28 17:50:31 +0530

    added  sudo access for mount cmd

--- efs_mount.sh
+++ efs_mount.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 mkdir ~/tokyo-efs-mount
 # Mounting Efs 
-mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  ~/tokyo-efs-mount
+sudo su - mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  ~/tokyo-efs-mount 
+echo ${efs_hostname} > /tmp/dns.txt
 cd ~/tokyo-efs-mount
 ls -al
 chmod go+rw .
