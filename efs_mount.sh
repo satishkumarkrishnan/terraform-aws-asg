1e0d4fc955b47b7991e7b58dfda801485a6e7317
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-29 08:47:38 +0530

    added the sudo permission

--- efs_mount.sh
+++ efs_mount.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-sudo su 
-mkdir ~/tokyo-efs-mount
+sudo mkdir ~/tokyo-efs-mount
 # Mounting Efs 
 #mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  ~/tokyo-efs-mount
 sudo mount -t nfs -o nfsvers=3,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/ /tokyo-efs-mount
 echo ${efs_hostname} > /tmp/dns.txt
-cd ~/tokyo-efs-mount
-ls -al
-chmod go+rw .
-touch test-file.txt  
+sudo cd /tokyo-efs-mount
+sudo ls -al
+sudo chmod go+rw .
+sudo touch test-file.txt  
 # Making Mount Permanent
 #echo ${efs_hostname}:/ /var/www/html nfs4 defaults,_netdev 0 0  | cat >> /etc/fstab
