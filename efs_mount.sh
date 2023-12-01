58cdc941802a1a8b7ca59568d10b026e5bb096c6
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-01 14:59:42 +0530

    updated the user_data script without sudo in efs_mount.sh script

--- efs_mount.sh
+++ efs_mount.sh
@@ -3,8 +3,8 @@
 mkdir /tmp/efs
 # Mounting Efs 
 #mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  ~/tokyo-efs-mount
-sudo echo ${efs_hostname} >> /tmp/efs_name
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/ /tmp/efs
+echo ${efs_hostname} >> /tmp/efs/efs_name
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/ /tmp/efs
 #echo ${efs_hostname} > /tmp/dns.txt
 #sudo cd /tokyo-efs-mount
 #sudo ls -al
