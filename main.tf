03b9eec33037ebbed70d957e731eded06b80a387
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-01 13:56:01 +0530

    adding the user_data script with data file option

--- main.tf
+++ main.tf
@@ -32,9 +32,10 @@ resource "aws_launch_template" "tokyo_launch_template" {
   #${templatefile("efs_mount.sh",{efs_hostname = aws_efs_file_system.tokyo_efs.dns_name})}    
   #EOF
 #)}"   
-  user_data = <<-EOT
+  user_data = "${base64encode(<<-EOT
     echo "efs_hostname = ${aws_efs_file_system.tokyo_efs.dns_name}" > /tmp/efs_dns
   EOT
+  )}"
   key_name      = "ec2-key"
   vpc_security_group_ids = [module.vpc.vpc_fe_sg]  
   depends_on = [aws_efs_file_system.tokyo_efs] 
