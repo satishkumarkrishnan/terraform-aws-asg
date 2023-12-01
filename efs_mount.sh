28411aa396b0237ccf509747acc2b1aaf1d5a4f8
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-01 20:00:00 +0530

    modified user data

--- efs_mount.sh
+++ efs_mount.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo su - 
+dnf install -y nfs-utils
 mkdir /tmp/efs
 echo ${efs_hostname} >> /tmp/efs/efs_name
+ systemctl enable --now nfs-server
 # Actually mount the EFS filesystem
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2 "${efs_hostname}:/" /tmp/efs
 # Mounting Efs 
