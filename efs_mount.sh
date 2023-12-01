1a24e66b4bb790cc66b5da5b3e7f1df0e8183d14
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-01 16:03:58 +0530

    updated the mount script in lin # 7

--- efs_mount.sh
+++ efs_mount.sh
@@ -4,7 +4,7 @@ mkdir /tmp/efs
 # Mounting Efs 
 #mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  ~/tokyo-efs-mount
 echo ${efs_hostname} >> /tmp/efs/efs_name
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport "${efs_hostname}":/ /tmp/efs
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport "${efs_hostname}:/" /tmp/efs
 #echo ${efs_hostname} > /tmp/dns.txt
 #sudo cd /tokyo-efs-mount
 #sudo ls -al
