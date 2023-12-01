f9f4523883176716c97ae8f2140925d9435abf93
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-01 17:07:46 +0530

    updated the efs script

--- efs_mount.sh
+++ efs_mount.sh
@@ -7,10 +7,3 @@ mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=
 # Mounting Efs 
 #mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  ~/tokyo-efs-mount
 #mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport "${efs_hostname}:/" /tmp/efs
-#echo ${efs_hostname} > /tmp/dns.txt
-#sudo cd /tokyo-efs-mount
-#sudo ls -al
-#sudo chmod go+rw .
-#sudo touch test-file.txt  */
-# Making Mount Permanent
-#echo ${efs_hostname}:/ /var/www/html nfs4 defaults,_netdev 0 0  | cat >> /etc/fstab
