bcc55d8f213c321015a8972197073b9e381adbba
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-01 19:33:48 +0530

    modified user data

--- main.tf
+++ main.tf
@@ -27,18 +27,17 @@ resource "aws_launch_template" "tokyo_launch_template" {
   name_prefix   = "tokyo_asg"
   image_id      = var.ami
   instance_type = var.instance_type  
-  #efs_hostname = aws_efs_file_system.tokyo_efs.dns_name
-  user_data = "${base64encode(<<EOF
+  #user_data = "${base64encode(<<EOF
 #!/bin/bash
-sudo su -  
-mkdir /tmp/efs
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2 ${aws_efs_file_system.tokyo_efs.dns_name}:/" /tmp/efs
-  EOF
-)}"   
-  #user_data = "${base64encode(<<-EOT
-  #  ${templatefile("efs_mount.sh",{efs_hostname = aws_efs_file_system.tokyo_efs.dns_name})}
-  #EOT
-  #)}"
+#sudo su -  
+#mkdir /tmp/efs
+#mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2 ${aws_efs_file_system.tokyo_efs.dns_name}:/" /tmp/efs
+#  EOF
+#)}"   
+  user_data = "${base64encode(<<-EOT
+    ${templatefile("efs_mount.sh",{efs_hostname = aws_efs_file_system.tokyo_efs.dns_name})}
+  EOT
+  )}"
   key_name      = "ec2-key"
   vpc_security_group_ids = [module.vpc.vpc_fe_sg]  
   depends_on = [aws_efs_file_system.tokyo_efs] 
