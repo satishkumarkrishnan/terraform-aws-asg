dccf4719822ae72beeda1b5c4f4ed8a35545669b
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-01 09:15:49 +0530

    updating the user data

--- efs_mount.sh
+++ efs_mount.sh
@@ -3,7 +3,7 @@ sudo su -
 mkdir /efs
 # Mounting Efs 
 #mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  ~/tokyo-efs-mount
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/ /efs
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/ efs
 #echo ${efs_hostname} > /tmp/dns.txt
 #sudo cd /tokyo-efs-mount
 #sudo ls -al
