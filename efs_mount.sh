4895ee74f3b09eadd66454b8627993e631558fe5
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-12 12:13:42 +0530

    added export cmd for efs

--- efs_mount.sh
+++ efs_mount.sh
@@ -6,7 +6,7 @@ echo ${efs_hostname} >> /tmp/efs/efs_name
 export efs_hostname=${efs_hostname}
 systemctl enable --now nfs-server
 # Actually mount the EFS filesystem
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "${efs_hostname}:/" /tmp/efs
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport ${efs_hostname}:/ /tmp/efs
 # Mounting Efs 
 #mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  ~/tokyo-efs-mount
 #mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport "${efs_hostname}:/" /tmp/efs
