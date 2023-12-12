aea7fbf62853af4688594239c37b26965eb5de96
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-12 16:29:15 +0530

    added the double quotes in ln # 10 of efs_mount.sh

--- efs_mount.sh
+++ efs_mount.sh
@@ -2,7 +2,8 @@
 sudo su - 
 dnf install -y nfs-utils
 mkdir /tmp/efs
-export echo $efs_hostname >> /tmp/efs/efs_name
+echo $efs_hostname >> /tmp/efs/efs_name
+export efs_hostname=${efs_hostname}
 systemctl enable --now nfs-server
 # Actually mount the EFS filesystem
 mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "$efs_hostname:/" /tmp/efs
