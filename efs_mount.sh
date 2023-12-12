dd16af01ee99f9cde1680f1f71abc97b592210bb
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-12 14:13:48 +0530

    added efs_hostname to efs_name.txt

--- efs_mount.sh
+++ efs_mount.sh
@@ -4,10 +4,10 @@ dnf install -y nfs-utils
 mkdir /tmp/efs
 echo ${efs_hostname} >> /tmp/efs/efs_name
 export efs_hostname=${efs_hostname}
-echo {efs_hostname} > /tmp/efs/efs_name.txt
+echo $efs_hostname >> /tmp/efs/efs_name.txt
 systemctl enable --now nfs-server
 # Actually mount the EFS filesystem
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport {efs_hostname}:/ /tmp/efs
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport $efs_hostname:/ /tmp/efs
 # Mounting Efs 
 #mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  ~/tokyo-efs-mount
 #mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport "${efs_hostname}:/" /tmp/efs
