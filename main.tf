9a5a63125f0446879023b51284f85b6c8d6e6d4b
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-12 12:31:01 +0530

    updated the EFS hostname var

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_launch_template" "tokyo_launch_template" {
 #  EOF
 #)}"   
   user_data = "${base64encode(<<-EOT
-    ${templatefile("efs_mount.sh",{efs_name = aws_efs_file_system.tokyo_efs.dns_name})}
+    ${templatefile("efs_mount.sh",{efs_hostname = aws_efs_file_system.tokyo_efs.dns_name})}
   EOT
   )}"
   key_name      = "ec2-key"
