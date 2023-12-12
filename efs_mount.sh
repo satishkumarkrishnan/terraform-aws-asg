4ff0882a568826731c34fbfea2e0efd1b6633870
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-12 11:51:38 +0530

    added export cmd for efs

--- efs_mount.sh
+++ efs_mount.sh
@@ -3,7 +3,8 @@ sudo su -
 dnf install -y nfs-utils
 mkdir /tmp/efs
 echo ${efs_hostname} >> /tmp/efs/efs_name
- systemctl enable --now nfs-server
+export efs_hostname=fs-0f4a48edafa2ea421.efs.ap-northeast-1.amazonaws.com
+systemctl enable --now nfs-server
 # Actually mount the EFS filesystem
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "${efs_hostname}:/" /tmp/efs
 # Mounting Efs 
