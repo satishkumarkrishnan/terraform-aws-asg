0425211fad4fafd9bb9d376796644c6f4cc2a548
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-12 11:11:29 +0530

    modified ami name

--- efs_mount.sh
+++ efs_mount.sh
@@ -5,7 +5,8 @@ mkdir /tmp/efs
 echo ${efs_hostname} >> /tmp/efs/efs_name
  systemctl enable --now nfs-server
 # Actually mount the EFS filesystem
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2 "${efs_hostname}:/" /tmp/efs
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "${efs_hostname}:/" /tmp/efs
 # Mounting Efs 
 #mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  ~/tokyo-efs-mount
 #mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport "${efs_hostname}:/" /tmp/efs
+#sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0f4a48edafa2ea421.efs.ap-northeast-1.amazonaws.com:/ efs
