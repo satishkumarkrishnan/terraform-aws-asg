2b8d9343307763f7a3ce63465072a5c5f58feaac
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-12 12:28:09 +0530

    renamed the efs name

--- data.tf
+++ data.tf
@@ -36,6 +36,6 @@ data "aws_availability_zones" "available" {
 data "template_file" "test" {
   template = "${file("${path.module}/efs_mount.sh")}" 
   vars = {  
-    efs_hostname = "${aws_efs_file_system.tokyo_efs.dns_name}"
+    efs_name = "${aws_efs_file_system.tokyo_efs.dns_name}"
   }
 }
