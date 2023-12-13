722bc7ca2fe78daf64b9223d0645f3ad691b755c
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-13 10:18:43 +0530

    added the sudo cmd to mount cmd

--- efs_mount.sh
+++ efs_mount.sh
@@ -6,7 +6,8 @@ echo ${efs_hostname} >> /tmp/efs/efs_name
 systemctl enable --now nfs-server
 # Actually mount the EFS filesystem
 #mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "efs_hostname:/" /tmp/efs
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "$(cat /tmp/efs/efs_name):/" /tmp/efs
+echo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "$(cat /tmp/efs/efs_name):/" /tmp/efs >> /tmp/efs_log
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "$(cat /tmp/efs/efs_name):/" /tmp/efs
 # Mounting Efs 
 #mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_hostname}:/  ~/tokyo-efs-mount
 #mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport "${efs_hostname}:/" /tmp/efs
