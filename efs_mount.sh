5316295ee4e11ae7bef1f36f7fe2c976736a42af
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-13 09:33:48 +0530

    added the user_data script

--- efs_mount.sh
+++ efs_mount.sh
@@ -3,9 +3,8 @@ sudo su -
 dnf install -y nfs-utils
 mkdir /tmp/efs
 echo ${efs_hostname} >> /tmp/efs/efs_name
-export efs_hostname=${efs_hostname}
-fs_id="$(cat /tmp/efs/efs_name)" 
-echo ${fs_id} >> /tmp/efs/fs_id
+#fs_id="$(cat /tmp/efs/efs_name)" 
+#echo ${fs_id} >> /tmp/efs/fs_id
 systemctl enable --now nfs-server
 # Actually mount the EFS filesystem
 #mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "efs_hostname:/" /tmp/efs
--- main.tf
+++ main.tf
@@ -34,10 +34,11 @@ resource "aws_launch_template" "tokyo_launch_template" {
 #mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2 ${aws_efs_file_system.tokyo_efs.dns_name}:/" /tmp/efs
 #  EOF
 #)}"   
-  user_data = "${base64encode(<<-EOT
-    ${templatefile("efs_mount.sh",{efs_hostname = aws_efs_file_system.tokyo_efs.dns_name})}
-  EOT
-  )}"
+  #user_data = "${base64encode(<<-EOT
+  #  ${templatefile("efs_mount.sh",{efs_hostname = aws_efs_file_system.tokyo_efs.dns_name})}
+  #EOT
+  #)}"
+  user_data = filebase64("${path.module}/efs_mount.sh")
   key_name      = "ec2-key"
   vpc_security_group_ids = [module.vpc.vpc_fe_sg]  
   depends_on = [aws_efs_file_system.tokyo_efs] 
