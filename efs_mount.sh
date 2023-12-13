1dfa420148f3698fa7a9c60b6501dd051f8c59fd
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-13 11:16:01 +0530

    added the sleep time

--- efs_mount.sh
+++ efs_mount.sh
@@ -3,9 +3,10 @@ sudo su -
 dnf install -y nfs-utils
 mkdir /tmp/efs
 echo ${efs_hostname} >> /tmp/efs/efs_name
+#export efs_id=${efs_hostname}
 systemctl enable --now nfs-server
 # Actually mount the EFS filesystem
-#mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "efs_hostname:/" /tmp/efs
+#mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "$efs_id:/" /tmp/efs
 echo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "$(cat /tmp/efs/efs_name):/" /tmp/efs >> /tmp/efs/efs_log
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,soft,timeo=600,retrans=2,noresvport "$(cat /tmp/efs/efs_name):/" /tmp/efs
 sleep 60s
