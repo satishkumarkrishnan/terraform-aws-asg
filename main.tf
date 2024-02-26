35e582ed01e137d71d80d66b95576505484e7a3f
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-02-26 14:06:14 +0530

    updated provider version

--- main.tf
+++ main.tf
@@ -4,11 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-<<<<<<< HEAD
-      version = "5.2.0"
-=======
       version = "5.1.0"
->>>>>>> 93e523d1c47ccf5559f058f5c544ef44bfa0559a
     }
   }
 }
