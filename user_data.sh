b884804c5ce82a0a1c2a22ac9772655236b9a718
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-03-18 11:18:13 +0530

    install containerd

--- user_data.sh
+++ user_data.sh
@@ -5,11 +5,11 @@
      #systemctl start httpd
      #systemctl enable httpd
      #echo "<h1>Terraform Learning from $(hostname -f)..</h1>" > /var/www/html/index.html    
-     git hub path - https://github.com/kubernetes/kubernetes     
-     sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/
-     sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg |sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
-     sudo swapoff -a
      sudo apt-get install -y containerd - install containerd
+     #sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/
+     #sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg |sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
+     #sudo swapoff -a
+     #sudo apt-get install -y containerd - install containerd
      
      #cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
      # overlay
@@ -22,14 +22,14 @@
      #net.ipv4.ip_forward                 = 1
      #EOF
 
-     sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo  gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-     echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    # sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo  gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    #echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
      
 
-     sleep 10s
-     sudo sysctl --system - apply sysctl without reboot
-     sudo apt-get install -y containerd
-     sudo containerd config default | sudo tee /etc/containerd/config.toml
-     sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
-     grep  'SystemdCgroup = true'  /etc/containerd/config.toml
-     sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+     #sleep 10s
+     #sudo sysctl --system - apply sysctl without reboot
+     #sudo apt-get install -y containerd
+     #sudo containerd config default | sudo tee /etc/containerd/config.toml
+     #sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+     #grep  'SystemdCgroup = true'  /etc/containerd/config.toml
+     #sudo apt-get install -y apt-transport-https ca-certificates curl gpg
