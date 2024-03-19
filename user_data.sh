0de246dcaac091763a8a2b48ce1a9aa517c149ee
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-03-19 12:11:01 +0530

    install httpd server

--- user_data.sh
+++ user_data.sh
@@ -4,8 +4,7 @@
      apt-get install httpd -y
      systemctl start httpd
      systemctl enable httpd
-     echo "<h1>Terraform Learning from $(hostname -f)..</h1>" > /var/www/html/index.html 
-     
+     echo "<h1>Terraform Learning from $(hostname -f)..</h1>" > /var/www/html/index.html      
      #sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/
      #sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg |sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
      #sudo swapoff -a
