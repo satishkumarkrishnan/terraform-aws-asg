fbb2b1f91693c32eb09c7f03e5bd85bf8f6529c7
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-04-03 14:47:12 +0530

    updated EFS config

--- main.tf
+++ main.tf
@@ -58,3 +58,13 @@ resource "aws_autoscaling_group" "tokyo_asg" {
   autoscaling_group_name = aws_autoscaling_group.tokyo_asg.name  
   depends_on = [aws_efs_file_system.tokyo_efs] 
 }
+
+
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
